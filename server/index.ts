7be15fa9fbd68a0bc5d2378b327dc81384c201d7
Leon Schrabeck <lschrabeck@web.de>
2020-09-07 16:38:08 +0200

    create new web project

--- server/index.ts
+++ server/index.ts
@@ -7,26 +7,28 @@ const port = 3001;
 app.get('/currentWeather', (req: any, res: any) => {
 	const dayInMilliseconds: number = 1;
     let openWeatherMapAPIKey = "e41b6bef2f0be05514166de5593c3f3e";
-	let latitude = 0;
-	let longitude = 0;
+	let latitude = 49.87167;
+	let longitude = 8.65027;
 	let timestampNow = Date.now();
 	let weatherData = new Array();
+	let dataSet;
 
-    let apiCallString = 'https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=' + latitude + '&lon=' + longitude;
+    let apiCallStringOutline = 'https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=' + latitude + '&lon=' + longitude;
+	let apiCallString;
 
-	for(let i = 0; i < 5; i++){
+	for(let i = 0; i < 5; i++) {
 		timestampNow -= dayInMilliseconds;
-		apiCallString = apiCallString + '&dt=' + timestampNow + '&appid=' + openWeatherMapAPIKey;
+		apiCallString = apiCallStringOutline + '&dt=' + timestampNow + '&appid=' + openWeatherMapAPIKey;
+
 		nodeFetch(apiCallString)
 		.then((response: any) => response.json())
-		.then((data: any) => { 
-			res.json(data);
-
+		.then((data: any) => {
+			dataSet = JSON.parse(data);
+			let relevantData = {temperature: 20, precipitation: 30}
+			weatherData[i] = relevantData;
 		})
 		.catch((error: any) => { console.error('Error:', error); });
 	}
-
-	
 });
 
 app.listen(port, () => { console.log(`Server listening at http://localhost:${port}`); });
